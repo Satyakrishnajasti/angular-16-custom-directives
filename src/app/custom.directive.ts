9cd16c5e77d0e476fd19485aa841e9ee93801098
Satytakrishnajasti <satyajasti9@gmail.com>
2023-07-11 21:23:05 +0530

    added

---
+++ src/app/custom.directive.ts
@@ -0,0 +1,92 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appCustom]',
+  standalone: true,
+})
+export class CustomDirective {
+  protected element = inject(ElementRef);
+  @HostBinding('style.color') color!: string;
+  @HostBinding('style.border-color') borderColor!: string;
+  @HostBinding('style.cursor') cursor = 'pointer';
+
+  possibleColors = [
+    'darksalmon',
+    'hotpink',
+    'lightskyblue',
+    'goldenrod',
+    'peachpuff',
+    'mediumspringgreen',
+    'cornflowerblue',
+    'blanchedalmond',
+    'lightslategrey',
+  ];
+
+  constructor() {
+    this.element.nativeElement.style.color = 'red';
+    this.element.nativeElement.style.fontSize = '20px';
+  }
+
+  @HostListener('click') click() {
+    const colorPick = Math.floor(Math.random() * this.possibleColors.length);
+
+    this.color = this.borderColor = this.possibleColors[colorPick];
+  }
+
+  // @HostListener('mouseover') onHover() {
+  //   this.element.nativeElement.style.cursor = 'pointer';
+  // }
+
+  // @HostListener('mouseout') mouseOut() {
+  //   alert('Mouse out');
+  // }
+
+  // @HostListener('mousedown') mouseDown() {
+  //   alert('Mouse Down');
+  // }
+
+  // @HostListener('mouseup') mouseUp() {
+  //   alert('Mouse up');
+  // }
+
+  // @HostListener('mousemove') mouseMove() {
+  //   alert('Mouse Move');
+  // }
+
+  // @HostListener('keypress') keyPress() {
+  //   alert('Keypress');
+  // }
+
+  // @HostListener('keyup') KeyUp() {
+  //   alert('KeyUp');
+  // }
+
+  // @HostListener('input') input() {
+  //   alert('input');
+  // }
+
+  // @HostListener('keydown') KeyDown() {
+  //   alert('KeyDown');
+  // }
+  // @HostListener('focus') focus() {
+  //   this.element.nativeElement.style.color = 'blue';
+  // }
+
+  // @HostListener('change') change() {
+  //   alert('Change');
+  // }
+
+  // @HostListener('blur') blur() {
+  //   alert('blur');
+  // }
+
+  // @HostListener('reset') reset() {
+  //   alert('reset');
+  // }
+}
